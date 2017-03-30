9ff7ce029f9eb1ca9e8ce81138f885baa8461922
Roy <royketelaar@gmail.com>
2017-03-30 19:10:49 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,22 @@
+System.register(['angular2/platform/browser', 'angular2/core', './main.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var browser_1, core_1, main_component_1;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (main_component_1_1) {
+                main_component_1 = main_component_1_1;
+            }],
+        execute: function() {
+            core_1.enableProdMode();
+            browser_1.bootstrap(main_component_1.MainComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.js.map
---
+++ app/main.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'my-app',
+	templateUrl: '../templates/main.html'
+})
+
+export class MainComponent {
+	
+}
